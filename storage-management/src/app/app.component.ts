cd9188f6266abb980b73f234a1b01561a713e5f5
MishaPuch <mpuczkowskij@gmail.com>
2024-03-02 22:21:01 +0100

    created main page

--- storage-management/src/app/app.component.ts
+++ storage-management/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './components/MainPage/child.components/app.child-components';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet ,
+    FormsModule ,
+    ChildComponent,
+    CardModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'storage-management';
+ name = '';
+
 }
