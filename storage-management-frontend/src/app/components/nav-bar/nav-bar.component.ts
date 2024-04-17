26b77e1c61129b920fc637d5ee6be8aee49d6692
MishaPuch <mpuczkowskij@gmail.com>
2024-04-17 18:42:12 +0200

    add menuburger

--- storage-management-frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ storage-management-frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
-  showMenu: boolean = false;
+  isMenuOpen: boolean = false; 
 
   toggleMenu() {
-    this.showMenu = !this.showMenu;
+    this.isMenuOpen = !this.isMenuOpen; 
   }
 }
